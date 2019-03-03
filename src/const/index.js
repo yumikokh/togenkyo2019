3678f5ec1241e2a910bd7ff6947fce12193303f5
kokubu-yumiko <kokubu-yumiko@kayac.com>
2019-03-03 12:06:52 +0900

    全部にわりあて

--- src/const/index.js
+++ src/const/index.js
@@ -1,4 +1,31 @@
 export const PRODUCTION = process.env.NODE_ENV === "production";
 
-export const tanbarinSound = ["do", "do2", "do3", "so", "so2"];
-export const drumSound = ["re", "re2", "fa", "fa2"];
+export const tanbarinSound = [
+  "do",
+  "do#",
+  "do2#",
+  "do2",
+  "do3",
+  "mi",
+  "mi2",
+  "so",
+  "so2",
+  "so#",
+  "so2#",
+  "si",
+  "si2"
+];
+export const drumSound = [
+  "re",
+  "re#",
+  "re2",
+  "re2#",
+  "fa",
+  "fa#",
+  "fa2",
+  "fa2#",
+  "ra",
+  "ra#",
+  "ra2",
+  "ra2#"
+];
